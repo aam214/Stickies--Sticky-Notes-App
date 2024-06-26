19f6e6e81132c15639f08daae0f1935eef27a75c
Ariana <152944814+aam214@users.noreply.github.com>
2024-06-26 18:58:28 -0500

    Added template of functions to be created later. Added event listeners for the icons of proceed and exit. Did querySelectors and getElementID for those to be targeted in the HTML.

---
+++ app.js
@@ -0,0 +1,33 @@
+let notesContainer = document.querySelector(".notes-container");
+let textContainer = document.querySelector(".text-container");
+let proceed = document.getElementById("proceed");
+let exit = document.getElementById("exit");
+
+var i = 0;
+
+exit.addEventListener("click", () => {
+exitNote();
+})
+
+
+proceed.addEventListener("click", () => {
+createNote();
+})
+
+function exitNote(){
+
+}
+
+function createNote(){
+  
+}
+
+function margin(){
+
+}
+function rotate(){
+
+}
+function color(){
+  
+}
