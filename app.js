fff611bc0ffad8cec3dcafced7391b7ec3f2e809
Ariana <152944814+aam214@users.noreply.github.com>
2024-06-26 19:20:17 -0500

    Added to the rotations function to give random rotations using Math.random multiplied by the length of the array. Added to the color function 6 different colors with a conditional statement and a return statement.

--- app.js
+++ app.js
@@ -24,12 +24,16 @@ function createNote(){
 
 function margin(){
 let margins = ["-5px","1px","5px","10px","15px","20px"]
-
 return margins[Math.floor(Math.random() * margins.length)];
 }
 function rotate(){
-
+  let rotations =["rotate(3deg)", "rotate(1deg)", "rotate(-1deg)", "rotate(-3deg)", "rotate(-5deg)", "rotate(-8deg)"];
+  return rotations[Math.floor(Math.random() * rotations.length)];
 }
 function color(){
-
+  let randomColors = ["#F7567C", "#99E1D9", "#93FF96", "#F79400", "#A833B9", "#C493FF"]
+if (i> randomColors.length - 1){
+  i = 0;
+}
+return randomColors[i++];
 }
